a73f59fa7067f9dbca1f0c49d8d1a7a269996384
Nuria Campo <nuria.campo7@gmail.com>
2024-10-16 11:28:16 +0200

    buscar/filtrar gatos

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 
 const ulList = document.querySelector(".js-list");
 
+const anastasioContent = 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asia al menos hace 500 años, donde tuvo su origen muy posiblemente.';
+
 const anastasio = `<li class="card">
             <article>
               <img
@@ -12,12 +14,12 @@ const anastasio = `<li class="card">
               <h3 class="card_title">Anastacio</h3>
               <h4 class="card_race">Siamés</h4>
               <p class="card_description">
-                Porte elegante, su patrón de color tan característico y sus ojos
-                de un azul intenso, pero su historia se remonta a Asia al menos
-                hace 500 años, donde tuvo su origen muy posiblemente.
+                ${anastasioContent}
               </p>
             </article>
           </li>`;
+
+const fionaContent = 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… Hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.'
 
 const fiona = `<li class="card">
             <img
@@ -28,12 +30,11 @@ const fiona = `<li class="card">
             <h3 class="card_title">Fiona</h3>
             <h4 class="card_race">Sphynx</h4>
             <p class="card_description">
-              Produce fascinación y curiosidad. Exótico, raro, bello, extraño…
-              Hasta con pinta de alienígena han llegado a definir a esta raza
-              gatuna que se caracteriza por la «ausencia» de pelo.
+              ${fionaContent}
             </p>
           </li>`;
 
+const cieloContent = 'Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.'
 
 const cielo = `<li class="card">
             <img
@@ -44,15 +45,13 @@ const cielo = `<li class="card">
             <h3 class="card_title">Cielo</h3>
             <h4 class="card_race">Maine Coon</h4>
             <p class="card_description">
-              Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-              bella mirada se ha convertido en una de sus señas de identidad.
-              Sus ojos son grandes y las orejas resultan largas y en punta.
+              ${cieloContent}
             </p>
           </li>`;
 
 ulList.innerHTML = anastasio + fiona + cielo;
 
-/*
+/* AÑADIR +
     1. Cuando hacemos click en + se tiene que quitar la clase collapsed
     2. Cuando hacemos click en cancelar tiene que aparecer de nuevo la clase collapsed
 */
@@ -72,4 +71,32 @@ buttonCancel.addEventListener('click', ()=> {
   newForm.classList.add('collapsed');
 })
 
+/* FILTRAS / BUSCAR
+1. Cuando la usuaria hace click en buscar:
+  - Recoger valor del input de la descripción.
+    - Comprobar si ese valor está incluido en la descripción de algún gato.
+      Si sí está incluido, elimina los que no
+*/
 
+
+const buttonSearch = document.querySelector('.js_button-search');
+const descripton = document.querySelector('.js_in_search_desc');
+
+buttonSearch.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.log('click');
+
+  const descriptionValue = descripton.value;
+
+  if (anastasioContent.includes(descriptionValue)) {
+    console.log('Soy anastasio');
+
+
+  }
+  if (fiona.includes(descriptionValue)) {
+    console.log('Soy fiona');
+  }
+  if (cielo.includes(descriptionValue)) {
+    console.log('Soy cielo');
+  }
+})
